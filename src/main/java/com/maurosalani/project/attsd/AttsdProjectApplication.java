85cd2b5111651ac52d6c9447a357539fb60f06d1
Lorenzo Salani <46.lore.46@gmail.com>
2019-06-15 16:21:48 +0200

    created project

---
+++ src/main/java/com/maurosalani/project/attsd/AttsdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.maurosalani.project.attsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttsdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttsdProjectApplication.class, args);
+	}
+
+}
