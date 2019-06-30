a6065973f84b84c6153011265885741185548b5f
Lorenzo Salani <46.lore.46@gmail.com>
2019-06-30 19:37:14 +0200

    added test for testFindAllWithExistingUser()

--- src/test/java/com/maurosalani/project/attsd/repositories/UserRepositoryTest.java
+++ src/test/java/com/maurosalani/project/attsd/repositories/UserRepositoryTest.java
@@ -1,10 +1,17 @@
 package com.maurosalani.project.attsd.repositories;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collection;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.maurosalani.project.attsd.model.User;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
@@ -13,9 +20,15 @@ public class UserRepositoryTest {
 	@Autowired
 	private UserRepository repository;
 
-	@Test
-	public void test() {
+	@Autowired
+	  private TestEntityManager entitymanager;
 
-	}
+	  @Test
+	  public void testFindAllWithExistingUser() {
+	    User user = new User(null, "test", "pwd", null, null, null);
+	    User saved = entitymanager.persistFlushFind(user);
+	    Collection<User> users = repository.findAll();
+	    assertThat(users).containsExactly(saved);
+	  }
 
 }
