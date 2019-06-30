37ea67b204c32ca08c669c87424d000e2b0318d5
Matteo Mauro <mauro.matteo96@gmail.com>
2019-06-30 19:16:47 +0200

    Updated User relationships with correct annotations (ManyToMany)
    User can follow many Users, besides User can be followed by many users: so there is a ManyToMany relationship with itself, involving two lists, followedUsers and followerUsers

--- src/main/java/com/maurosalani/project/attsd/model/User.java
+++ src/main/java/com/maurosalani/project/attsd/model/User.java
@@ -3,11 +3,14 @@ package com.maurosalani.project.attsd.model;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -17,27 +20,32 @@ public class User {
 	@GeneratedValue
 	private Long id;
 
+	@Column(unique = true)
 	private String username;
 	private String password;
 
-	@OneToMany
-	@JoinTable(name = "followers")
-	@JoinColumn(name = "person_id", referencedColumnName = "id")
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "followers_relation")
+	@JoinColumn(name = "follower_id", referencedColumnName = "id")
 	@JoinColumn(name = "followed_id", referencedColumnName = "id")
 	private List<User> followedUsers;
 
-	@OneToMany
+	@ManyToMany(mappedBy = "followedUsers")
+	private List<User> followerUsers;
+
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Game> games;
 
 	public User() {
 
 	}
 
-	public User(Long id, String username, String password, List<User> followedUsers, List<Game> games) {
+	public User(Long id, String username, String password, List<User> followedUsers, List<User> followerUsers, List<Game> games) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.followedUsers = followedUsers;
+		this.followerUsers = followerUsers;
 		this.games = games;
 	}
 
@@ -45,44 +53,58 @@ public class User {
 		return id;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getUsername() {
 		return username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public List<User> getFollowed() {
-		return followedUsers;
-	}
-
-	public List<Game> getGames() {
-		return games;
 	}
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	public String getPassword() {
+		return password;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public void setFollowed(List<User> followed) {
-		this.followedUsers = followed;
+	public List<User> getFollowedUsers() {
+		return followedUsers;
 	}
 
-	public void addFollowed(User user) {
-		followedUsers.add(user);
+	public void setFollowedUsers(List<User> followedUsers) {
+		this.followedUsers = followedUsers;
+	}
+
+	public List<User> getFollowerUsers() {
+		return followerUsers;
+	}
+
+	public void setFollowerUsers(List<User> followerUsers) {
+		this.followerUsers = followerUsers;
+	}
+
+	public List<Game> getGames() {
+		return games;
 	}
 
 	public void setGames(List<Game> games) {
 		this.games = games;
 	}
 
-	public void addGame(Game game) {
-		games.add(game);
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", games=" + games + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(followedUsers, followerUsers, games, id, password, username);
 	}
 
 	@Override
