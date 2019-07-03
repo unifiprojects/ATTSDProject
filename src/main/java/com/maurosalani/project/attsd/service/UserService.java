a632a37590f077ebfc3b78673372305b5a160ce1
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-03 23:40:14 +0200

    Implemented method getAllUsers()

--- src/main/java/com/maurosalani/project/attsd/service/UserService.java
+++ src/main/java/com/maurosalani/project/attsd/service/UserService.java
@@ -2,15 +2,24 @@ package com.maurosalani.project.attsd.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.maurosalani.project.attsd.model.User;
+import com.maurosalani.project.attsd.repository.UserRepository;
 
 @Service
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	public List<User> getAllUsers() {
-		return null;
+		return userRepository.findAllUsers();
 	}
 
 }
