2489116b1dd232047b37cadce4ee1f25d3c4a4d2
Lorenzo Salani <46.lore.46@gmail.com>
2019-07-03 17:58:55 +0200

    implemented testFindFollowedOfUserByUsername()

--- src/main/java/com/maurosalani/project/attsd/repositories/UserRepository.java
+++ src/main/java/com/maurosalani/project/attsd/repositories/UserRepository.java
@@ -15,5 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	@Query("select u.games from User u where u.username = ?1")
 	List<Game> findGamesOfUserByUsername(String username);
 
+	@Query("select u.followedUsers from User u where u.username = ?1")
 	List<User> findFollowedOfUserByUsername(String string);
 }
