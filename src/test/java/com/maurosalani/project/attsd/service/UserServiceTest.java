12b8f53e3f6aa8cf73fcefe5f6fb6d44ac950bb7
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-03 23:43:12 +0200

    Added test testFindAllUsersWhitExistingUsers()

--- src/test/java/com/maurosalani/project/attsd/service/UserServiceTest.java
+++ src/test/java/com/maurosalani/project/attsd/service/UserServiceTest.java
@@ -2,6 +2,7 @@ package com.maurosalani.project.attsd.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+import static java.util.Arrays.asList;
 
 import java.util.Collections;
 
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.maurosalani.project.attsd.model.User;
 import com.maurosalani.project.attsd.repository.UserRepository;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -26,6 +28,14 @@ public class UserServiceTest {
 	public void testFindAllUsersWhenDatabaseIsEmpty() {
 		when(userRepository.findAllUsers()).thenReturn(Collections.emptyList());
 		assertThat(userService.getAllUsers()).isEmpty();
+	}
+
+	@Test
+	public void testFindAllUsersWhitExistingUsers() {
+		User user1 = new User(1l, "username1", "pwd1");
+		User user2 = new User(2l, "username2", "pwd2");
+		when(userRepository.findAllUsers()).thenReturn(asList(user1, user2));
+		assertThat(userService.getAllUsers()).containsExactly(user1, user2);
 	}
 
 }
