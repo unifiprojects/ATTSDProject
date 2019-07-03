58000437a24140616c45a64d67acb69994728172
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-03 17:46:20 +0200

    Implemented test findGamesOfUserByUsername

--- src/main/java/com/maurosalani/project/attsd/repositories/UserRepository.java
+++ src/main/java/com/maurosalani/project/attsd/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.maurosalani.project.attsd.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.maurosalani.project.attsd.model.Game;
 import com.maurosalani.project.attsd.model.User;
@@ -10,7 +11,7 @@ import com.maurosalani.project.attsd.model.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByUsername(String string);
-	
-	
+
+	@Query("select u.games from User u where u.username = ?1")
 	List<Game> findGamesOfUserByUsername(String username);
 }
