ae43cdf1ba342220449a3494eeab2a70740f3fa3
Lorenzo Salani <46.lore.46@gmail.com>
2019-07-04 16:55:15 +0200

    implemented testFindUsersListOfGameByUsername()

--- src/main/java/com/maurosalani/project/attsd/repositories/GameRepository.java
+++ src/main/java/com/maurosalani/project/attsd/repositories/GameRepository.java
@@ -3,6 +3,7 @@ package com.maurosalani.project.attsd.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.maurosalani.project.attsd.model.Game;
 import com.maurosalani.project.attsd.model.User;
@@ -13,6 +14,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
 
 	Game findByNameLike(String string);
 
+	@Query("select u.users from Game u where u.name = ?1")
 	List<User> findUsersOfGameByName(String string);
 
 }
