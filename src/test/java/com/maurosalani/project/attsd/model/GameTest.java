7b54e355e4916340562da0617c5450c5701fd5aa
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-04 18:58:31 +0200

    Added tests for add methods of User and Game model classes

---
+++ src/test/java/com/maurosalani/project/attsd/model/GameTest.java
@@ -0,0 +1,26 @@
+package com.maurosalani.project.attsd.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public void testAddUser() {
+		Game game = new Game(null, "game_name", "game_description", new Date());
+		User user = new User(null, "user", "pwd_user");
+		game.addUser(user);
+		assertThat(game.getUsers()).containsExactly(user);
+	}
+	
+	@Test
+	public void testAddUserWithArgumentNull() {
+		Game game = new Game(null, "game_name", "game_description", new Date());
+		game.addUser(null);
+		assertThat(game.getUsers()).isNull();
+	}
+
+}
