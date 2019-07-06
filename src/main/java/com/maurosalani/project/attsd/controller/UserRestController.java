5d92cf74eb8b617040b17efbed150ada98abd218
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-06 19:38:59 +0200

    Implemented method getAllUsers()

--- src/main/java/com/maurosalani/project/attsd/controller/UserRestController.java
+++ src/main/java/com/maurosalani/project/attsd/controller/UserRestController.java
@@ -1,8 +1,23 @@
 package com.maurosalani.project.attsd.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.maurosalani.project.attsd.model.User;
+import com.maurosalani.project.attsd.service.UserService;
 
 @RestController
 public class UserRestController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/api/users")
+	public List<User> getAllUsers() {
+		return userService.getAllUsers();
+	}
 
 }
