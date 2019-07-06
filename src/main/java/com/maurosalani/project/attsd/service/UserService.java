bffbdd13af10d6889cce8238854a5b5959bb28af
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-06 19:37:40 +0200

    Added test testFindAllUsersWithEmptyDatabase()

---
+++ src/main/java/com/maurosalani/project/attsd/service/UserService.java
@@ -0,0 +1,17 @@
+package com.maurosalani.project.attsd.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.maurosalani.project.attsd.model.User;
+
+@Service
+public class UserService {
+	private static final String TEMPORARY_IMPLEMENTATION = "Temporary implementation";
+
+	public List<User> getAllUsers() {
+		throw new UnsupportedOperationException(TEMPORARY_IMPLEMENTATION);
+	}
+
+}
--- src/test/java/UserRestControllerTest.java
+++ src/test/java/UserRestControllerTest.java
@@ -1,12 +1,18 @@
-import static org.junit.Assert.fail;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.maurosalani.project.attsd.controller.UserRestController;
+import com.maurosalani.project.attsd.service.UserService;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 
@@ -16,14 +22,24 @@ public class UserRestControllerTest {
 	@InjectMocks
 	private UserRestController userRestController;
 
+	@Mock
+	private UserService userService;
+
 	@Before
 	public void setup() {
 		RestAssuredMockMvc.standaloneSetup(userRestController);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindAllUsersWithEmptyDatabase() throws Exception {
+		when(userService.getAllUsers()).thenReturn(Collections.emptyList());
+		
+		given().
+		when().
+			get("/api/users").
+		then().
+			statusCode(200).
+			assertThat().body(is("[]"));
 	}
 
 }
