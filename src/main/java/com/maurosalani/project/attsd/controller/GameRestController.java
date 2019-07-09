e1019b2a707383efdf3feb5aa2a61058958089db
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-09 22:07:13 +0200

    Implemented end-point "/api/games/update/{id}"

--- src/main/java/com/maurosalani/project/attsd/controller/GameRestController.java
+++ src/main/java/com/maurosalani/project/attsd/controller/GameRestController.java
@@ -7,12 +7,15 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maurosalani.project.attsd.exception.BadRequestException;
 import com.maurosalani.project.attsd.exception.GameNotFoundException;
+import com.maurosalani.project.attsd.exception.GameNotFoundException;
+import com.maurosalani.project.attsd.model.Game;
 import com.maurosalani.project.attsd.model.Game;
 import com.maurosalani.project.attsd.service.GameService;
 
@@ -48,7 +51,10 @@ public class GameRestController {
 		return gameService.insertNewGame(game);
 	}
 	
-	
+	@PutMapping(path = "/update/{id}", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Game updateGameById(@PathVariable Long id, @RequestBody Game game) throws GameNotFoundException {
+		return gameService.updateGameById(id, game);
+	}
 	
 	
 	
