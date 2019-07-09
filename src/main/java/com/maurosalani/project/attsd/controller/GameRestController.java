01a392086bf2f0acf2475d22dd0e6d52b78e91ec
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-09 21:55:48 +0200

    Implemented end-point "/api/games/new"

--- src/main/java/com/maurosalani/project/attsd/controller/GameRestController.java
+++ src/main/java/com/maurosalani/project/attsd/controller/GameRestController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +43,10 @@ public class GameRestController {
 		return gameService.getGamesByNameLike(name);
 	}
 	
-	
+	@PostMapping(path = "/new", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Game insertNewGame(@RequestBody Game game) {
+		return gameService.insertNewGame(game);
+	}
 	
 	
 	
