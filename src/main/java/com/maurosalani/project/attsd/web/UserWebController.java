38a610318a8beffae2e19e934b1fc9b9c0c60614
Lorenzo Salani <46.lore.46@gmail.com>
2019-07-11 15:35:53 +0200

    initialized web repository, started with UserWebController

---
+++ src/main/java/com/maurosalani/project/attsd/web/UserWebController.java
@@ -0,0 +1,8 @@
+package com.maurosalani.project.attsd.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserWebController {
+
+}
---
+++ src/test/java/com/maurosalani/project/attsd/web/UserWebControllerTest.java
@@ -0,0 +1,19 @@
+package com.maurosalani.project.attsd.web;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = UserWebController.class)
+public class UserWebControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
