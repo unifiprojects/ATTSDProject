4d665b96c33b6a0169fa621213a7e61109bf4ab2
Lorenzo Salani <46.lore.46@gmail.com>
2019-07-13 18:44:51 +0200

    implemented testProfile_UsersFound()

--- src/main/java/com/maurosalani/project/attsd/web/UserWebController.java
+++ src/main/java/com/maurosalani/project/attsd/web/UserWebController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.maurosalani.project.attsd.exception.UserNotFoundException;
@@ -105,6 +106,13 @@ public class UserWebController {
 			}
 		}
 		return "search";
+	}
+
+	@GetMapping("/profile/{username}")
+	public String profile(@PathVariable String username, Model model) {
+		User user = userService.getUserByUsername(username);
+		model.addAttribute("user", user);
+		return "profile";
 	}
 
 	private boolean isAlreadyLogged(HttpSession session) {
