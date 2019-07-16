79f8c95e9f0d249e85a7597cf6eaafdf0c841c23
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-16 18:16:52 +0200

    Added lists to model classes

--- src/main/java/com/maurosalani/project/attsd/model/Game.java
+++ src/main/java/com/maurosalani/project/attsd/model/Game.java
@@ -1,7 +1,7 @@
 package com.maurosalani.project.attsd.model;
 
 import java.util.Date;
-import java.util.Objects;
+import java.util.List;
 
 public class Game {
 
@@ -10,16 +10,27 @@ public class Game {
 	private String name;
 	private String description;
 	private Date releaseDate;
+	private List<User> users;
 
 	public Game() {
 
 	}
 
 	public Game(Long id, String name, String description, Date releaseDate) {
+		super();
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.releaseDate = releaseDate;
+	}
+
+	public Game(Long id, String name, String description, Date releaseDate, List<User> users) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.releaseDate = releaseDate;
+		this.users = users;
 	}
 
 	public Long getId() {
@@ -34,24 +45,44 @@ public class Game {
 		return name;
 	}
 
-	public String getDescription() {
-		return description;
-	}
-
-	public Date getReleaseDate() {
-		return releaseDate;
-	}
-
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
 	public void setReleaseDate(Date releaseDate) {
 		this.releaseDate = releaseDate;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((releaseDate == null) ? 0 : releaseDate.hashCode());
+		result = prime * result + ((users == null) ? 0 : users.hashCode());
+		return result;
 	}
 
 	@Override
@@ -63,19 +94,32 @@ public class Game {
 		if (getClass() != obj.getClass())
 			return false;
 		Game other = (Game) obj;
-		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
-				&& Objects.equals(name, other.name) && Objects.equals(releaseDate, other.releaseDate);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(description, id, name, releaseDate);
-	}
-
-	@Override
-	public String toString() {
-		return "Game [id=" + id + ", name=" + name + ", description=" + description + ", releaseDate=" + releaseDate
-				+ "]";
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (releaseDate == null) {
+			if (other.releaseDate != null)
+				return false;
+		} else if (!releaseDate.equals(other.releaseDate))
+			return false;
+		if (users == null) {
+			if (other.users != null)
+				return false;
+		} else if (!users.equals(other.users))
+			return false;
+		return true;
 	}
 
 }
--- src/main/java/com/maurosalani/project/attsd/model/User.java
+++ src/main/java/com/maurosalani/project/attsd/model/User.java
@@ -1,20 +1,37 @@
 package com.maurosalani.project.attsd.model;
 
+import java.util.List;
+
 public class User {
 
 	private Long id;
 
 	private String username;
 	private String password;
+	private List<User> followedUsers;
+	private List<User> followerUsers;
+	private List<Game> games;
 
 	public User() {
 
 	}
 
 	public User(Long id, String username, String password) {
+		super();
 		this.id = id;
 		this.username = username;
 		this.password = password;
+	}
+
+	public User(Long id, String username, String password, List<User> followedUsers, List<User> followerUsers,
+			List<Game> games) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.followedUsers = followedUsers;
+		this.followerUsers = followerUsers;
+		this.games = games;
 	}
 
 	public Long getId() {
@@ -41,10 +58,37 @@ public class User {
 		this.password = password;
 	}
 
+	public List<User> getFollowedUsers() {
+		return followedUsers;
+	}
+
+	public void setFollowedUsers(List<User> followedUsers) {
+		this.followedUsers = followedUsers;
+	}
+
+	public List<User> getFollowerUsers() {
+		return followerUsers;
+	}
+
+	public void setFollowerUsers(List<User> followerUsers) {
+		this.followerUsers = followerUsers;
+	}
+
+	public List<Game> getGames() {
+		return games;
+	}
+
+	public void setGames(List<Game> games) {
+		this.games = games;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((followedUsers == null) ? 0 : followedUsers.hashCode());
+		result = prime * result + ((followerUsers == null) ? 0 : followerUsers.hashCode());
+		result = prime * result + ((games == null) ? 0 : games.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((password == null) ? 0 : password.hashCode());
 		result = prime * result + ((username == null) ? 0 : username.hashCode());
@@ -60,6 +104,21 @@ public class User {
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
+		if (followedUsers == null) {
+			if (other.followedUsers != null)
+				return false;
+		} else if (!followedUsers.equals(other.followedUsers))
+			return false;
+		if (followerUsers == null) {
+			if (other.followerUsers != null)
+				return false;
+		} else if (!followerUsers.equals(other.followerUsers))
+			return false;
+		if (games == null) {
+			if (other.games != null)
+				return false;
+		} else if (!games.equals(other.games))
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
