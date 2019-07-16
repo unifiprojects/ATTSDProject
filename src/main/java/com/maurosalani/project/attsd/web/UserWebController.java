b5e89c177554609cb892d3b962d77cf046734d70
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-16 22:42:04 +0200

    Moved RegistrationForm to package dto and updated pom to exclude from coverage

--- src/main/java/com/maurosalani/project/attsd/web/UserWebController.java
+++ src/main/java/com/maurosalani/project/attsd/web/UserWebController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.maurosalani.project.attsd.dto.RegistrationForm;
 import com.maurosalani.project.attsd.exception.GameNotFoundException;
 import com.maurosalani.project.attsd.exception.LoginFailedException;
 import com.maurosalani.project.attsd.exception.PasswordRequiredException;
