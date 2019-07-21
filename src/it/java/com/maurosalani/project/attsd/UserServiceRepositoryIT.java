2f2f20e8a759ede1867fe1d92e67b0b5247e79b9
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-21 20:22:44 +0200

    Added IT testServiceCanDeleteFromRepository()

--- src/it/java/com/maurosalani/project/attsd/UserServiceRepositoryIT.java
+++ src/it/java/com/maurosalani/project/attsd/UserServiceRepositoryIT.java
@@ -1,15 +1,16 @@
 package com.maurosalani.project.attsd;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.maurosalani.project.attsd.exception.UsernameAlreadyExistingException;
 import com.maurosalani.project.attsd.model.User;
 import com.maurosalani.project.attsd.repository.UserRepository;
 import com.maurosalani.project.attsd.service.UserService;
@@ -17,6 +18,7 @@ import com.maurosalani.project.attsd.service.UserService;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @Import(UserService.class)
+@ActiveProfiles("mysql")
 public class UserServiceRepositoryIT {
 
 	@Autowired
@@ -28,7 +30,16 @@ public class UserServiceRepositoryIT {
 	@Test
 	public void testServiceCanInsertIntoRepository() throws Exception {
 		User saved = userService.insertNewUser(new User(null, "user", "password"));
-		userRepository.findById(saved.getId()).isPresent();
+		assertTrue(userRepository.findById(saved.getId()).isPresent());
 	}
+	
+	@Test
+	public void testServiceCanDeleteFromRepository() throws Exception {
+		User saved = userService.insertNewUser(new User(null, "user", "password"));
+		assertTrue(userRepository.findById(saved.getId()).isPresent());
+		userService.deleteById(saved.getId());
+		assertFalse(userRepository.findById(saved.getId()).isPresent());
+	}
+	
 
 }
