5487dfe12a7959a332279f9a1baa7bc665ede2d3
Matteo Mauro <mauro.matteo96@gmail.com>
2019-07-25 20:32:44 +0200

    Changed version of Jackson using bom 2.7.9 (see description for the reason)
    SpringBoot 2.1.6 uses Jacskon 2.9.9, but the deserialization performed by Jackson seems to be afflicted by a bug (https://stackoverflow.com/questions/47701937/what-version-of-jackson-does-spring-boot-use, see also https://github.com/FasterXML/jackson-databind/issues/1599). Within the versions fixed so far, there is the 2.7.9. The problem happened in the deserialization of the followedUsers list of a user in the database, as happened with the test testAddFollowedUser_ShouldBeUpdatedWithSuccess()

--- src/e2e/java/com/maurosalani/project/attsd/UserRestControllerE2E.java
+++ src/e2e/java/com/maurosalani/project/attsd/UserRestControllerE2E.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import com.maurosalani.project.attsd.dto.CredentialsDTO;
-
+import com.maurosalani.project.attsd.dto.UpdateAddFollowedUserFormDTO;
 import com.maurosalani.project.attsd.dto.UpdatePasswordUserFormDTO;
 import com.maurosalani.project.attsd.dto.UserDTO;
 import com.maurosalani.project.attsd.model.User;
@@ -138,4 +138,47 @@ public class UserRestControllerE2E {
 		assertThat(responseFind.getBody().as(User.class).getPassword()).isEqualTo("newPassword");
 	}
 	
+	@Test
+	public void testAddFollowedUser_ShouldBeUpdatedWithSuccess() {
+		UserDTO userDto = new UserDTO(null, "username", "password");
+		UpdateAddFollowedUserFormDTO form = new UpdateAddFollowedUserFormDTO();
+		form.setCredentials(new CredentialsDTO("username", "password"));
+		UserDTO followedToAddDTO = new UserDTO(null, "followed", "followed_pwd");
+		
+		Response responseNew = 
+				given().
+					contentType(MediaType.APPLICATION_JSON_VALUE).
+					body(userDto).
+				when().
+					post("/api/users/new");
+		User saved = responseNew.getBody().as(User.class);
+		 
+		Response responseNewFollowed = 
+				given().
+					contentType(MediaType.APPLICATION_JSON_VALUE).
+					body(followedToAddDTO).
+				when().
+					post("/api/users/new");
+		User savedFollowed = responseNewFollowed.getBody().as(User.class);
+		form.setFollowedToAdd(savedFollowed);
+		
+		Response responseAddFollowed = 
+				given().
+					contentType(MediaType.APPLICATION_JSON_VALUE).
+					body(form).
+				when().
+					patch("/api/users/update/addFollowedUser/" + saved.getId());
+		
+		assertThat(responseAddFollowed.getStatusCode()).isEqualTo(HttpStatus.OK.value());
+		
+		Response responseFind = 
+				given().
+				when().
+					get("/api/users/id/" + saved.getId());
+		
+		assertThat(responseFind.getStatusCode()).isEqualTo(HttpStatus.OK.value());
+		User updated = responseFind.getBody().as(User.class);
+		assertThat(updated.getPassword()).isEqualTo("password");
+		assertThat(updated.getFollowedUsers().get(0).getUsername()).isEqualTo("followed");
+	}
 }
