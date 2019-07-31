905fc76d9c910c369ae1b1bd74c795fdb1452f78
Lorenzo Salani <46.lore.46@gmail.com>
2019-07-31 23:14:27 +0200

    fixed pit with exclusion for web_config

--- src/main/java/com/maurosalani/project/attsd/web_config/WebConfig.java
+++ src/main/java/com/maurosalani/project/attsd/web_config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.maurosalani.project.attsd.web;
+package com.maurosalani.project.attsd.web_config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
