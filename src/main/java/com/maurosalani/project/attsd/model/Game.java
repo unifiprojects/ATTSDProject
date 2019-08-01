13617e2789ab5e977b9ad8a5bd3cfcfdc6b6e184
Matteo Mauro <mauro.matteo96@gmail.com>
2019-08-01 10:27:00 +0200

    Removed unused import

--- src/main/java/com/maurosalani/project/attsd/model/Game.java
+++ src/main/java/com/maurosalani/project/attsd/model/Game.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
